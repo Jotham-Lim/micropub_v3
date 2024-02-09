64d9424d82b14224cb50a051c852c02462f24041
Jotham-Lim <admin@jothamlim.com>
2024-02-09 16:51:38 +0800

    asd

---
+++ CHANGELOG.md
@@ -0,0 +1,64 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+This project adheres to [Semantic Versioning](http://semver.org/).
+
+## 0.7.0 – YYYY-MM-DD
+
+* **Breaking** – now requires at least Node.js 8.x
+
+## 0.6.1 – 2018-01-20
+
+* **Feature** – layout names are now configurable and can also be disabled. Fixes #29
+
+## 0.6.0 – 2017-05-16
+
+* **Breaking** – the format for configuring content-dependent categories have changed, now a JSON array
+* **Feature** – permalink styles and filename styles can now be content-dependent just like categories
+
+## 0.5.0 – 2017-05-14
+
+* **Feature** – post file names are now configurable
+* **Feature** – media file names are now configurable
+* **Bug fix** – corrected resolving of site options
+* **Bug fix** – corrected combined use of `MICROPUB_SITES_JSON` and `MICROPUB_SITE_URL`
+* **Updated** – dependencies has been updated
+
+## 0.4.4 – 2017-02-14
+
+* **Improvement** – includes updated `micropub-express` module that supports the [now standard](https://github.com/voxpelli/node-micropub-express/issues/7) `create` IndieAuth scope, that eg. Quill [now uses](https://github.com/aaronpk/Quill/commit/eab1a65f63f227bae126a554e3bf93aa05c70695)
+* **Updated** – dependencies has been updated
+
+## 0.4.3 – 2017-01-28
+
+* **Feature** – support `config` queries and include syndication targets in it
+
+## 0.4.2 – 2017-01-28
+
+* **Feature** – configurable syndication targets, see `sample.env` and #9
+* **Improvement** – using new [microformat-express](https://github.com/voxpelli/node-micropub-express) version that is more spec compliant: JSON by default in query responses, better errors and support for space separated scopes
+* **Updated** – dependencies has been updated
+
+## 0.4.1 – 2017-01-21
+
+* **Improvement** – using new `format-microformat` version that by default publish all posts in the past by 15 seconds to avoid time sync issues with build servers
+* **Improvement** – now includes a Yarn lock file
+* **Updated** – dependencies has been updated
+
+## 0.4.0 - 2016-08-17
+
+* **Feature** – derviced categories are now configurable
+* **Somewhat breaking** – this project now requires Node.js 6.x
+
+## 0.3.1 - 2016-07-30
+
+* **Bug fix** – the caching of the auto-configuration didn't work
+
+## 0.3.0 - 2016-07-30
+
+* **Feature** – permalinks are now configurable
+* **Feature** – this project now autoconfigures eg. the permalink style based on a sites `_config.yml` file
+* **Somewhat breaking** – as this project now respects the actual permalink configuration it will now default to other permalink styles than before
+
+## 0.2.0 - 2016-07-06
+
+- Initial public release
---
+++ test/handler.spec.js
@@ -0,0 +1,511 @@
+'use strict';
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const nock = require('nock');
+const sinon = require('sinon');
+
+chai.use(chaiAsPromised);
+chai.should();
+
+describe('Handler', () => {
+  const handler = require('../lib/handler.js');
+  let handlerConfig;
+  let clock;
+
+  const basicTest = function ({ content, message, finalUrl, filename, contentInput } = {}) {
+    const token = 'abc123';
+    const user = 'username';
+    const repo = 'repo';
+
+    filename = filename || '_posts/2015-06-30-awesomeness-is-awesome.md';
+
+    const path = '/repos/' + user + '/' + repo + '/contents/' + filename;
+
+    const encodedContent = Buffer.from(content || (
+      '---\n' +
+      'layout: micropubpost\n' +
+      'date: \'2015-06-30T14:19:45.000Z\'\n' +
+      'title: awesomeness is awesome\n' +
+      'lang: en\n' +
+      'slug: awesomeness-is-awesome\n' +
+      '---\n' +
+      'hello world\n'
+    )).toString('base64');
+
+    const mock = nock('https://api.github.com/')
+      .matchHeader('authorization', val => val && val[0] === 'Bearer ' + token)
+      .put(path, {
+        message: message || 'uploading article',
+        content: encodedContent
+      })
+      .reply(201, { content: { sha: 'abc123' } });
+
+    return handler(
+      {
+        token,
+        user,
+        repo
+      },
+      {
+        type: ['h-entry'],
+        properties: {
+          content: [contentInput || 'hello world'],
+          name: ['awesomeness is awesome'],
+          lang: ['en']
+        }
+      },
+      'http://example.com/foo/',
+      handlerConfig
+    )
+      .then(url => {
+        mock.done();
+        url.should.equal(finalUrl || 'http://example.com/foo/2015/06/awesomeness-is-awesome/');
+      });
+  };
+
+  beforeEach(() => {
+    nock.cleanAll();
+    nock.disableNetConnect();
+    clock = sinon.useFakeTimers(1435674000000);
+    handlerConfig = {
+      // Enable to help with debugging of wrongly formatted content
+      // verbose: true,
+      noAutoConfigure: true,
+      permalinkStyle: '/:categories/:year/:month/:title/'
+    };
+  });
+
+  afterEach(() => {
+    clock.restore();
+    sinon.restore();
+    if (!nock.isDone()) {
+      throw new Error('pending nock mocks: ' + nock.pendingMocks());
+    }
+  });
+
+  describe('main', () => {
+    it('should format and send content', () => {
+      return basicTest();
+    });
+
+    it('should upload files prior to content', () => {
+      const token = 'abc123';
+      const user = 'username';
+      const repo = 'repo';
+      const repoPath = '/repos/' + user + '/' + repo + '/contents/';
+      const mediaFilename = 'foo.jpg';
+
+      const fileContent = Buffer.from('abc123');
+
+      const encodedContent = Buffer.from(
+        '---\n' +
+        'layout: micropubpost\n' +
+        'date: \'2015-06-30T14:19:45.000Z\'\n' +
+        'title: awesomeness is awesome\n' +
+        'lang: en\n' +
+        'slug: awesomeness-is-awesome\n' +
+        'mf-photo:\n' +
+        '  - \'http://example.com/foo/media/2015-06-awesomeness-is-awesome/' + mediaFilename + '\'\n' +
+        '---\n' +
+        'hello world\n'
+      );
+
+      const mock = nock('https://api.github.com/')
+        .matchHeader('authorization', val => val && val[0] === 'Bearer ' + token)
+        // Upload of the media
+        .put(repoPath + 'media/2015-06-awesomeness-is-awesome/' + mediaFilename, {
+          // TODO: Change this commit message to at least be "new media" instead
+          message: 'uploading media',
+          content: fileContent.toString('base64')
+        })
+        .reply(201, { content: { sha: 'abc123' } })
+        // Upload of the content
+        .put(repoPath + '_posts/2015-06-30-awesomeness-is-awesome.md', {
+          message: 'uploading article',
+          content: encodedContent.toString('base64')
+        })
+        .reply(201, { content: { sha: 'abc123' } });
+
+      return handler(
+        {
+          token,
+          user,
+          repo
+        }, {
+          type: ['h-entry'],
+          properties: {
+            content: ['hello world'],
+            name: ['awesomeness is awesome'],
+            lang: ['en']
+          },
+          files: {
+            photo: [{ filename: 'foo.jpg', buffer: fileContent }]
+          }
+        },
+        'http://example.com/foo/',
+        handlerConfig
+      )
+        .then(url => {
+          mock.done();
+          url.should.equal('http://example.com/foo/2015/06/awesomeness-is-awesome/');
+        });
+    });
+
+    it('should override existing content if matching URL', () => {
+      const token = 'abc123';
+      const user = 'username';
+      const repo = 'repo';
+      const path = '/repos/' + user + '/' + repo + '/contents/_posts/2015-06-30-awesomeness-is-awesome.md';
+      const sha = 'abc123';
+
+      const encodedContent = Buffer.from(
+        '---\n' +
+        'layout: micropubpost\n' +
+        'date: \'2015-06-30T14:19:45.000Z\'\n' +
+        'title: awesomeness is awesome\n' +
+        'lang: en\n' +
+        'slug: awesomeness-is-awesome\n' +
+        '---\n' +
+        'hello world\n'
+      );
+      const base64 = encodedContent.toString('base64');
+
+      const mock = nock('https://api.github.com/')
+        .put(path, {
+          message: 'uploading article',
+          content: base64
+        })
+        .reply(422, {})
+
+        .get(path)
+        .reply(200, { sha })
+
+        .put(path, {
+          message: 'uploading article',
+          content: base64,
+          sha
+        })
+        .reply(201, { content: { sha: 'xyz789' } });
+
+      return handler(
+        {
+          token,
+          user,
+          repo
+        }, {
+          type: ['h-entry'],
+          url: 'http://example.com/foo/2015/06/awesomeness-is-awesome/',
+          properties: {
+            content: ['hello world'],
+            name: ['awesomeness is awesome'],
+            lang: ['en']
+          }
+        },
+        'http://example.com/foo/',
+        handlerConfig
+      )
+        .then(url => {
+          mock.done();
+          url.should.equal('http://example.com/foo/2015/06/awesomeness-is-awesome/');
+        });
+    });
+
+    it('should not override existing content if no matching URL', () => {
+      const token = 'abc123';
+      const user = 'username';
+      const repo = 'repo';
+      const path = '/repos/' + user + '/' + repo + '/contents/_posts/2015-06-30-awesomeness-is-awesome.md';
+
+      const encodedContent = Buffer.from(
+        '---\n' +
+        'layout: micropubpost\n' +
+        'date: \'2015-06-30T14:19:45.000Z\'\n' +
+        'title: awesomeness is awesome\n' +
+        'lang: en\n' +
+        'slug: awesomeness-is-awesome\n' +
+        '---\n' +
+        'hello world\n'
+      );
+      const base64 = encodedContent.toString('base64');
+
+      const mock = nock('https://api.github.com/')
+        .put(path, {
+          message: 'uploading article',
+          content: base64
+        })
+        .reply(422, {});
+
+      return handler(
+        {
+          token,
+          user,
+          repo
+        }, {
+          type: ['h-entry'],
+          properties: {
+            content: ['hello world'],
+            name: ['awesomeness is awesome'],
+            lang: ['en']
+          }
+        },
+        'http://example.com/foo/',
+        handlerConfig
+      )
+        .then(url => {
+          mock.done();
+          url.should.equal(false);
+        });
+    });
+
+    it('should set a custom commit message when formatter returns a category', () => {
+      const token = 'abc123';
+      const user = 'username';
+      const repo = 'repo';
+      const path = '/repos/' + user + '/' + repo + '/contents/_posts/2015-06-30-51585.md';
+
+      const encodedContent = Buffer.from(
+        '---\n' +
+        'layout: micropubpost\n' +
+        'date: \'2015-06-30T14:19:45.000Z\'\n' +
+        'title: \'\'\n' +
+        'lang: en\n' +
+        'slug: \'51585\'\n' +
+        'category: social\n' +
+        '---\n' +
+        'hello world\n'
+      );
+      const base64 = encodedContent.toString('base64');
+
+      const mock = nock('https://api.github.com/')
+        .matchHeader('authorization', val => val && val[0] === 'Bearer ' + token)
+        .put(path, {
+          message: 'uploading social interaction',
+          content: base64
+        })
+        .reply(201, { content: { sha: 'abc123' } });
+
+      return handler(
+        {
+          token,
+          user,
+          repo
+        }, {
+          type: ['h-entry'],
+          properties: {
+            content: ['hello world'],
+            lang: ['en']
+          }
+        },
+        'http://example.com/foo/',
+        handlerConfig
+      )
+        .then(url => {
+          mock.done();
+          url.should.equal('http://example.com/foo/social/2015/06/51585/');
+        });
+    });
+
+    it('should format HTML to Markdown and send content', () => {
+      const token = 'abc123';
+      const user = 'username';
+      const repo = 'repo';
+      const path = '/repos/' + user + '/' + repo + '/contents/_posts/2015-06-30-awesomeness-is-awesome.md';
+
+      const encodedContent = Buffer.from(
+        '---\n' +
+        'layout: micropubpost\n' +
+        'date: \'2015-06-30T14:19:45.000Z\'\n' +
+        'title: awesomeness is awesome\n' +
+        'lang: en\n' +
+        'slug: awesomeness-is-awesome\n' +
+        '---\n' +
+        '**hello world**\n'
+      );
+
+      const mock = nock('https://api.github.com/')
+        .matchHeader('authorization', val => val && val[0] === 'Bearer ' + token)
+        .put(path, {
+          message: 'uploading article',
+          content: encodedContent.toString('base64')
+        })
+        .reply(201, { content: { sha: 'abc123' } });
+
+      return handler(
+        {
+          token,
+          user,
+          repo
+        }, {
+          type: ['h-entry'],
+          properties: {
+            content: [{
+              html: '<strong>hello world</strong>',
+              value: 'hello world'
+            }],
+            name: ['awesomeness is awesome'],
+            lang: ['en']
+          }
+        },
+        'http://example.com/foo/',
+        handlerConfig
+      )
+        .then(url => {
+          mock.done();
+          url.should.equal('http://example.com/foo/2015/06/awesomeness-is-awesome/');
+        });
+    });
+
+    it('should support category deriving', () => {
+      handlerConfig.deriveCategory = [
+        { value: 'foo', condition: 'abc = 123 AND foo = bar' },
+        { value: 'xyz', condition: 'content[] = "hello world"' }
+      ];
+
+      return basicTest({
+        content: (
+          '---\n' +
+          'layout: micropubpost\n' +
+          'date: \'2015-06-30T14:19:45.000Z\'\n' +
+          'title: awesomeness is awesome\n' +
+          'lang: en\n' +
+          'slug: awesomeness-is-awesome\n' +
+          'category: xyz\n' +
+          '---\n' +
+          'hello world\n'
+        ),
+        message: 'uploading xyz',
+        finalUrl: 'http://example.com/foo/xyz/2015/06/awesomeness-is-awesome/'
+      });
+    });
+
+    it('should support custom layout deriving', () => {
+      handlerConfig.layoutName = [
+        { value: 'foo', condition: 'abc = 123 AND foo = bar' },
+        { value: 'xyz', condition: 'content[] = "hello world"' }
+      ];
+
+      return basicTest({
+        content: (
+          '---\n' +
+          'layout: xyz\n' +
+          'date: \'2015-06-30T14:19:45.000Z\'\n' +
+          'title: awesomeness is awesome\n' +
+          'lang: en\n' +
+          'slug: awesomeness-is-awesome\n' +
+          '---\n' +
+          'hello world\n'
+        )
+      });
+    });
+
+    it('should support simple custom layout', () => {
+      handlerConfig.layoutName = 'simple';
+
+      return basicTest({
+        content: (
+          '---\n' +
+          'layout: simple\n' +
+          'date: \'2015-06-30T14:19:45.000Z\'\n' +
+          'title: awesomeness is awesome\n' +
+          'lang: en\n' +
+          'slug: awesomeness-is-awesome\n' +
+          '---\n' +
+          'hello world\n'
+        )
+      });
+    });
+
+    it('should support layout-less', () => {
+      handlerConfig.layoutName = false;
+
+      return basicTest({
+        content: (
+          '---\n' +
+          'date: \'2015-06-30T14:19:45.000Z\'\n' +
+          'title: awesomeness is awesome\n' +
+          'lang: en\n' +
+          'slug: awesomeness-is-awesome\n' +
+          '---\n' +
+          'hello world\n'
+        )
+      });
+    });
+
+    it('should support callback based permalink style', () => {
+      handlerConfig.permalinkStyle = [
+        { value: 'first/:slug', condition: 'content[] = "hello world"' },
+        { value: 'second/:slug', condition: 'abc = 123 AND foo = bar' }
+      ];
+
+      return basicTest({
+        content: (
+          '---\n' +
+          'layout: micropubpost\n' +
+          'date: \'2015-06-30T14:19:45.000Z\'\n' +
+          'title: awesomeness is awesome\n' +
+          'lang: en\n' +
+          'slug: awesomeness-is-awesome\n' +
+          '---\n' +
+          'hello world\n'
+        ),
+        finalUrl: 'http://example.com/foo/first/awesomeness-is-awesome'
+      });
+    });
+
+    it('should support callback based filename style', () => {
+      handlerConfig.filenameStyle = [
+        { value: 'first/:slug', condition: 'abc = 123 AND foo = bar' },
+        { value: 'second/:slug', condition: 'content[] = "hello world"' }
+      ];
+
+      return basicTest({
+        content: (
+          '---\n' +
+          'layout: micropubpost\n' +
+          'date: \'2015-06-30T14:19:45.000Z\'\n' +
+          'title: awesomeness is awesome\n' +
+          'lang: en\n' +
+          'slug: awesomeness-is-awesome\n' +
+          '---\n' +
+          'hello world\n'
+        ),
+        filename: 'second/awesomeness-is-awesome.md'
+      });
+    });
+
+    it('should correctly HTML-encode text input', () => {
+      return basicTest({
+        contentInput: 'world < hello',
+        content: (
+          '---\n' +
+          'layout: micropubpost\n' +
+          'date: \'2015-06-30T14:19:45.000Z\'\n' +
+          'title: awesomeness is awesome\n' +
+          'lang: en\n' +
+          'slug: awesomeness-is-awesome\n' +
+          '---\n' +
+          'world &lt; hello\n'
+        )
+      });
+    });
+
+    it('should support HTML-encode opt out', () => {
+      handlerConfig.encodeHTML = false;
+
+      return basicTest({
+        contentInput: 'world < hello',
+        content: (
+          '---\n' +
+          'layout: micropubpost\n' +
+          'date: \'2015-06-30T14:19:45.000Z\'\n' +
+          'title: awesomeness is awesome\n' +
+          'lang: en\n' +
+          'slug: awesomeness-is-awesome\n' +
+          '---\n' +
+          'world < hello\n'
+        )
+      });
+    });
+  });
+});
